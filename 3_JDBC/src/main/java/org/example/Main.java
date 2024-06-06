f7673e88e1e063078ed836f4eb6ea4cf32a9dd51
Rjt <cdrrjt@gmail.com>
2024-06-06 19:04:36 +0530

    Till Trasction Handling

--- 3_JDBC/src/main/java/org/example/Main.java
+++ 3_JDBC/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
+import javax.xml.stream.events.StartElement;
 import java.sql.*;
+import java.util.Arrays;
 
 public class Main {
     private static final String url = "jdbc:mysql://localhost:3306/dbforjdbc";
@@ -9,7 +11,7 @@ public class Main {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch(ClassNotFoundException e){
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
         try{
@@ -18,22 +20,63 @@ public class Main {
 
             Connection con = DriverManager.getConnection(url,username,password);
             Statement statement = con.createStatement();
-            String query = "select * from students";
-            // This will hold the table
-            ResultSet result = statement.executeQuery(query);
+//            String query = String.format("DELETE from students where id = %o OR %o",3,4);
+//
+//            // This will hold the table
+//            int rowAffected = statement.executeUpdate(query);
+//
+//            if( rowAffected > 0 ) System.out.println("Successfully Updated");
 
-            while( result.next() ){
-                  int id = result.getInt("id");
-                  double marks = result.getDouble("marks");
-                  String name = result.getString("name");
-                  int age = result.getInt("age");
+            printTable(con,statement);
+            multiStatement(con);
 
-                System.out.println("ID : " + id + ", Name : " + name + ", Marks : " + marks + ", Age : " + age);
-            }
+
         }catch(SQLException s){
-            s.printStackTrace();
+            System.out.println(s.getMessage());
         }
 
     }
+
+    public static void printTable(Connection con, Statement statement) throws SQLException{
+        ResultSet result = statement.executeQuery("SELECT * from students");
+
+        while( result.next() ){
+            int id = result.getInt("id");
+            double marks = result.getDouble("marks");
+            String name = result.getString("name");
+            int age = result.getInt("age");
+
+            System.out.println("ID : " + id + ", Name : " + name + ", Marks : " + marks + ", Age : " + age);
+        }
+    }
+
+    // USing a single Prepared Statement to run multiple queries
+    public static  void multiStatement(Connection con) throws SQLException{
+
+         String query = "INSERT INTO students(name,age,marks) VALUE(?,?,?)";
+         PreparedStatement preparedStatement = con.prepareStatement(query);
+
+         // Adding To batch
+         preparedStatement.setString(1,"Rahul");
+         preparedStatement.setInt(2,23);
+         preparedStatement.setDouble(3,96.6);
+         preparedStatement.addBatch();
+
+         //Now using the same preparedStatement and adding it to the batch
+        preparedStatement.setString(1,"Rajat");
+        preparedStatement.setInt(2,20);
+        preparedStatement.setDouble(3,94.6);
+        preparedStatement.addBatch();
+
+        preparedStatement.setString(1,"Dwivedi");
+        preparedStatement.setInt(2,20);
+        preparedStatement.setDouble(3,75.5);
+        preparedStatement.addBatch();
+
+        int[] rowsAffected  = preparedStatement.executeBatch();
+
+        System.out.printf(Arrays.toString(rowsAffected));
+    }
+
 }
 
