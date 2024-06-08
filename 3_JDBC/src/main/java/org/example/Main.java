da03dcfc4d90a6cc46f7d67d5f6243ac93ef0383
Rjt <cdrrjt@gmail.com>
2024-06-08 17:36:19 +0530

    On linux Jdbc Update

--- 3_JDBC/src/main/java/org/example/Main.java
+++ 3_JDBC/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Step 2 : (after adding mysql-connector ) load drivers
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
