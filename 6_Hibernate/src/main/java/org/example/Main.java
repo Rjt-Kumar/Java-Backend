651ae938d2e6aab1a384566e7f286c7406deca19
Rjt <cdrrjt@gmail.com>
2024-07-20 21:34:06 +0530

    Spring Core Notes Added

--- 6_Hibernate/src/main/java/org/example/Main.java
+++ 6_Hibernate/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+
+        try {
+            System.out.println("Hello world!");
+            Configuration conf = new Configuration();
+            conf.configure("hibernate.cnf.xml");
+            SessionFactory factory = conf.buildSessionFactory();
+            System.out.println(factory);
+//            System.out.println(factory.isClosed());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
